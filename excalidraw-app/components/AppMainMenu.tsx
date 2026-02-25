62feb16aaa3efa799d319fd4a4a7872c929989b6
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2026-02-25 13:14:16 +0300

    fix: correct export dialog visibility logic

--- excalidraw-app/components/AppMainMenu.tsx
+++ excalidraw-app/components/AppMainMenu.tsx
@@ -21,7 +21,7 @@ export const AppMainMenu: React.FC<{
     <MainMenu>
       <MainMenu.DefaultItems.LoadScene />
       <MainMenu.DefaultItems.SaveToActiveFile />
-      {/* <MainMenu.DefaultItems.Export /> */}
+      <MainMenu.DefaultItems.Export />
       <MainMenu.DefaultItems.SaveAsImage />
       {/* {props.isCollabEnabled && (
         <MainMenu.DefaultItems.LiveCollaborationTrigger
