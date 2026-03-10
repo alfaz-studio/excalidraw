4068c3758aab511e1343e50246611b293ebcc616
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2026-03-10 15:22:45 +0300

    feat(toolbar): replace more tools icon with vertical ellipsis

--- packages/excalidraw/components/icons.tsx
+++ packages/excalidraw/components/icons.tsx
@@ -1840,9 +1840,9 @@ export const eyeDropperIcon = createIcon(
 export const extraToolsIcon = createIcon(
   <g strokeWidth={1.5}>
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <path d="M12 3l-4 7h8z"></path>
-    <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-    <path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+    <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
   </g>,
   tablerIconProps,
 );
